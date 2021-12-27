68ee1d65a88e222d94fa29f31ad92ba733a96674
James Brind <jb753@cam.ac.uk>
2021-12-27 11:38:26 +0000

    Work on perpendicular thickness

--- shapespace.py
+++ shapespace.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.special import binom
 import matplotlib.pyplot as plt
 from numpy.linalg import lstsq
+from scipy.optimize import newton
 
 def read_seglid(file_name):
     with open(file_name, 'r') as f:
@@ -11,11 +12,15 @@ def read_seglid(file_name):
     xy = np.array([[float(i) for i in line.strip().split()] for line in lines]).T
     # Find the turning point of x coordinate
     i1 = np.where(np.diff(xy[0,:])>0.)[0][0]
-    # Determine axial chord and normalise
-    c = np.ptp(xy[0,:])
     # Split into pressure and suction sides, normalise
-    xy1 = np.flip(xy[:,:i1],1)/c
-    xy2 = xy[:,i1:]/c
+    xy1 = np.flip(xy[:,:(i1+1)],1)
+    xy2 = xy[:,i1:-1]
+    # Determine axial chord and normalise
+    xle = np.mean((xy1[0,0],xy2[0,0]))
+    xte = np.mean((xy1[0,-1],xy2[0,-1]))
+    c = xte-xle
+    xy1 /= c
+    xy2 /= c
     return xy1, xy2
 
 def bernstein( x, n, i ):
@@ -56,23 +61,41 @@ def fit_coeffs( x, s, order, dx=0., Rle=None ):
     else:
         return lstsq(X, strim, rcond=None)[:2]
 
-
-def fit_section( xy, order, zte, dx=0. ):
-    # Initially fit with free leading edge radii
-    s = [z_to_shape_space(*xyi, zte) for xyi in xy]
-    A, _ = zip(*[fit_coeffs( xyi[0,:], si, order, dx ) for xyi, si in zip(xy, s) ])
-    A0 = np.array([np.abs(Ai[0]) for Ai in A])
-    Rle = 0.5*A0**2.
-    guess_Rle = np.mean(Rle)
-    err_Rle = np.ptp(Rle)/guess_Rle
-    A = [fit_coeffs( xyi[0,:], si, order, dx, guess_Rle*np.sign(xyi[1,2]) )[0] for xyi, si in zip(xy, s) ]
-    return A
-
+# def fit_section( xy, order, zte, dx=0. ):
+#     # Initially fit with free leading edge radii
+#     s = [z_to_shape_space(*xyi, zte) for xyi in xy]
+#     A, _ = zip(*[fit_coeffs( xyi[0,:], si, order, dx ) for xyi, si in zip(xy, s) ])
+#     A0 = np.array([np.abs(Ai[0]) for Ai in A])
+#     Rle = 0.5*A0**2.
+#     guess_Rle = np.mean(Rle)
+#     err_Rle = np.ptp(Rle)/guess_Rle
+#     A = [fit_coeffs( xyi[0,:], si, order, dx, guess_Rle*np.sign(xyi[1,2]) )[0] for xyi, si in zip(xy, s) ]
+#     return A
 
 def resample_coeffs( A, order ):
     x = np.linspace(0.,1.)
     s = evaluate_coeffs( x, A )
     return fit_coeffs( x, s, order )
+
+def eval_camber( x, chi ):
+    tanchi = np.tan(np.radians(chi))
+    return x * tanchi[0] + 0.5*(tanchi[1]-tanchi[0])*x*x
+
+def eval_camber_slope( x, chi ):
+    tanchi = np.tan(np.radians(chi))
+    return tanchi[0] + (tanchi[1]-tanchi[0])*x
+
+def coord_to_thickness( xy, chi ):
+    """Perpendicular thickness distribution given camber line angles."""
+    xu, yu = xy
+    dyu_dx = np.gradient(yu, xu)
+    tanchi = np.tan(np.radians(chi))
+    # Find intersections of xu, yu with camber line perpendicular
+    def iterate( x ):
+        return yu - eval_camber( x, chi ) + (xu - x)/eval_camber_slope(x, chi)
+    xc = newton( iterate, xu )
+    yc = eval_camber( xc, chi )
+    return xc, yc
 
 
 if __name__=="__main__":
@@ -83,14 +106,15 @@ if __name__=="__main__":
     zte = 0.00
     order = 4
     dx = 0.05
+    chi = (15.,-15.)
 
-    A = fit_section( xy, order, zte, dx )
+    xc1, yc1 = coord_to_thickness( xy[0], chi)
 
-
+    # s = [z_to_shape_space(*xyi, zte) for xyi in xy]
+    # A, resid = zip(*[fit_coeffs( xyi[0,:], si, order, dx ) for xyi, si in zip(xy, s) ])
     xfit =  np.linspace(0.,1.,10000)
-    sfit = [evaluate_coeffs( xfit, Ai ) for Ai in A]
-
-    yfit = [ z_from_shape_space( xfit, sfiti, zte) for sfiti in sfit]
+    # sfit = [evaluate_coeffs( xfit, Ai ) for Ai in A]
+    # yfit = [ z_from_shape_space( xfit, sfiti, zte) for sfiti in sfit]
 
     # x = np.linspace(0,1.)
     # s = - (x**3 - x**2. + .2*x)
@@ -100,11 +124,12 @@ if __name__=="__main__":
 
     f, a = plt.subplots()
     # f2, a2 = plt.subplots()
-    for xyi, sfiti, yfiti in zip(xy, sfit, yfit):
+    for xyi in xy:
         # a2.plot(xyi[0,:],si,'-')
         # a2.plot(xfit,sfiti,'--')
-        a.plot(*xyi,'kx')
-        a.plot( xfit, yfiti, 'k-')
+        a.plot(*xyi,'x')
+    a.plot( xfit, eval_camber(xfit, chi), 'k--')
+    a.plot( xc1, yc1, 'o')
     # a.plot(*xy2,'kx')
     a.axis('equal')
     # a2.axis('equal')
